5982ea907658dce95f3fbdbd01eb97e3aa74c0a7
Vampire421 <91833744+Vampire421@users.noreply.github.com>
2021-10-04 10:18:58 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,12 +8,12 @@ async def start(client,message):
 	insert(int(message.chat.id))
 	await message.reply_text(text =f"""
 	Hello {message.from_user.first_name }
-	__I am file renamer bot, Please sent any telegram 
-	**Document Or Video** and enter new filenameto rename it__
+	__I am file renamer bot, Please sent any Telegram
+	**Documents or Files**I Was Made By @saminsumesh
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/housefull_links") ], 
+	[InlineKeyboardButton("Subscribe ⚡", url="https://t.me/housefull_films") ]  ]))
 
 
 
